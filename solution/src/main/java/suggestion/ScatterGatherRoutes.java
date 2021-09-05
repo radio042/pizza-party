5d7b5312cbb7239315eb178b68ae1a9677b5ea10
radio042 <nikolaev.radi.fau@gmail.com>
2021-09-05 09:55:53 +0200

    wip: scatter-gather route definitions

--- solution/src/main/java/suggestion/ScatterGatherRoutes.java
+++ solution/src/main/java/suggestion/ScatterGatherRoutes.java
@@ -1,15 +1,36 @@
 package suggestion;
 
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 public class ScatterGatherRoutes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        // todo implement scatter route
-        from("todo").to("todo");
+        from("kafka:suggestions?brokers=localhost:29092")
+                .routeId("scatter")
+                .multicast()
+                .to("kafka:arthur?brokers=localhost:29092")
+                .to("kafka:ford?brokers=localhost:29092")
+                .to("kafka:zaphod?brokers=localhost:29092");
 
-        // todo implement gather route
-        from("todo").to("todo");
+        from("kafka:responses?brokers=localhost:29092")
+                .routeId("gather")
+                .aggregate(newConsensusStrategy())
+                .body()
+                .completionSize(3)
+                .to("mock:result");
+    }
+
+    private AggregationStrategy newConsensusStrategy() {
+
+        return new AggregationStrategy() {
+            @Override
+            public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+                // todo
+                return null;
+            }
+        };
     }
 }
