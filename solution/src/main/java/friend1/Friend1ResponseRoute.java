1d7739293c0c25ad4172e3d8286ea8aa4aabeb54
radio042 <nikolaev.radi.fau@gmail.com>
2021-09-11 20:07:05 +0200

    fixed friend 1 response route

--- solution/src/main/java/friend1/Friend1ResponseRoute.java
+++ solution/src/main/java/friend1/Friend1ResponseRoute.java
@@ -1,14 +1,7 @@
 package friend1;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.apache.camel.AggregationStrategy;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.kafka.common.protocol.types.Field;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Friend1ResponseRoute extends RouteBuilder {
 
@@ -16,38 +9,22 @@ public class Friend1ResponseRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("kafka:suggestions?brokers=localhost:29092")
                 .routeId("friend1")
-                .process(addPizzaTypeHeader())
-                .pollEnrich("file:data?noop=true&fileName=friend-1-pizza-preferences.txt")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.out.println(exchange.getIn().getBody(String.class));
-                    }
-                })
-                .process(createResponse())
+                .setHeader("pizza-type")
+                .jsonpath("$.pizza-type", String.class)
+                .pollEnrich()
+                .simple("file:classes?noop=true&fileName=friend-1-pizza-preferences.txt")
+                .aggregationStrategy(comparePizzaTypeAndRenderResponse())
                 .to("kafka:responses?brokers=localhost:29092");
     }
 
-    private Processor createResponse() {
-        return new Processor() {
-            @Override
-            public void process(Exchange exchange) throws Exception {
-                System.out.println("########## createResponse");
-                String pizzaType = exchange.getIn().getHeader("pizza-type", String.class);
-                String preference = exchange.getIn().getBody(String.class).contains(pizzaType) ? "yep" : "nope";
-                String response = String.format("{\"pizza-type\": \"%s\", \"response\": \"%s\"}", pizzaType, preference);
-                exchange.getIn().setBody(response);
-            }
-        };
-    }
-
-    private Processor addPizzaTypeHeader() {
-        return new Processor() {
-            @Override
-            public void process(Exchange exchange) throws Exception {
-                System.out.println("########## addPizzaTypeHeader");
-                exchange.getIn().setHeader("pizza-type", simple("\"${in.body.pizza-type}\""));
-            }
+    private AggregationStrategy comparePizzaTypeAndRenderResponse() {
+        return (oldExchange, newExchange) -> {
+            String pizzaType = oldExchange.getIn().getHeader("pizza-type", String.class);
+            String preferences = newExchange.getIn().getBody(String.class);
+            String preference = preferences.contains(pizzaType) ? "yep" : "nope";
+            String response = String.format("{\"pizza-type\": \"%s\", \"response\": \"%s\"}", pizzaType, preference);
+            newExchange.getIn().setBody(response);
+            return newExchange;
         };
     }
 
