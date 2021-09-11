3c9063476debb9588f98ad3f600055ed07a14cb9
radio042 <nikolaev.radi.fau@gmail.com>
2021-09-11 22:51:29 +0200

    refactor: method names

--- solution/src/main/java/scattergather/GatherRoute.java
+++ solution/src/main/java/scattergather/GatherRoute.java
@@ -1,7 +1,7 @@
 package scattergather;
 
 import org.apache.camel.AggregationStrategy;
-import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 public class GatherRoute extends RouteBuilder {
@@ -12,13 +12,13 @@ public class GatherRoute extends RouteBuilder {
                 .routeId("gather")
                 .setHeader("pizza-type").jsonpath("$.pizza-type", String.class)
                 .setHeader("approval").jsonpath("$.approval", Boolean.class)
-                .aggregate(header("pizza-type"), newConsensusStrategy())
+                .aggregate(header("pizza-type"), checkConsensus())
                 .completionSize(2) // because we expect a response from 2 friends
-                .process(toResponse())
+                .process(this::renderResponse)
                 .to("kafka:out?brokers=localhost:29092");
     }
 
-    private AggregationStrategy newConsensusStrategy() {
+    private AggregationStrategy checkConsensus() {
         return (oldExchange, newExchange) -> {
             boolean approvalInNewResponse = newExchange.getIn().getHeader("approval", Boolean.class);
             if (oldExchange == null) {
@@ -32,14 +32,12 @@ public class GatherRoute extends RouteBuilder {
         };
     }
 
-    private Processor toResponse() {
-        return exchange -> {
-            boolean consensus = exchange.getIn().getBody(Boolean.class);
-            String pizzaType = exchange.getIn().getHeader("pizza-type", String.class);
-            String transformedMessage = String.format("%s sind mit %s einverstanden.",
-                    consensus ? "Alle" : "Nicht alle",
-                    pizzaType);
-            exchange.getIn().setBody(transformedMessage);
-        };
+    private void renderResponse(Exchange exchange) {
+        boolean consensus = exchange.getIn().getBody(Boolean.class);
+        String pizzaType = exchange.getIn().getHeader("pizza-type", String.class);
+        String transformedMessage = String.format("%s sind mit %s einverstanden.",
+                consensus ? "Alle" : "Nicht alle",
+                pizzaType);
+        exchange.getIn().setBody(transformedMessage);
     }
 }
